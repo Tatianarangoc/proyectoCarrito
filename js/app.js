d8bb1010db7c0acc095f7c85996d850065152df4
Tatiana <tatianarangoc@gmail.com>
2023-05-03 12:16:48 +0200

    added array and objetc

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
 //Variables
 const carrito = document.querySelector("#carrito");
 const listaCursos = document.querySelector("#lista-cursos");
-const listaCarrito = document.querySelector("#lista-carrio");
+const contenedorCarrito = document.querySelector("#lista-carrito tbody");
 const vaciarCarrito = document.querySelector("#vaciar-carrito")
+let articulosCarrito = [];
 
 
 handleFunctions();
@@ -16,14 +17,14 @@ function agregarCurso(e) {
     e.preventDefault();
 
     if (e.target.classList.contains("agregar-carrito")) {
-        const cursoSeleccionado = e.target.parentElement.parentElement;
-        leerDatos(cursoSeleccionado);
+        const curso = e.target.parentElement.parentElement;
+        leerDatosCurso(curso);
     }
 }
 
 //Lee el contenido del HTML al que le dimos cick y extrae la informacon del curso
-function leerDatos(curso) {
-    console.log(curso);
+function leerDatosCurso(curso) {
+    //console.log(curso);
 
 
     //crear un objeto con el contenido actual
@@ -31,9 +32,42 @@ function leerDatos(curso) {
         imagen: curso.querySelector("img").src,
         titulo: curso.querySelector("h4").textContent,
         precio: curso.querySelector(".precio span").textContent,
-        id: curso.querySelector("a").getAtttribute("data-id"),
-
+        id: curso.querySelector("a").getAttribute("data-id"),
+        cantidad: 1
     }
-    console.log(infoCurso)
+    //Agregar elementos al arreglo carrito
+    articulosCarrito = [...articulosCarrito, infoCurso];
+
+    console.log(articulosCarrito);
+
+    carritoHTML();
 }
 
+//Muestra el carrito de compras en el HTML
+function carritoHTML() {
+
+    //Limpiar el Html
+    limpiarHTML();
+
+    //Recorre el carrito y genera el HTML
+    articulosCarrito.forEach(curso => {
+        const row = document.createElement("tr");
+        row.innerHTML = `
+            <td>
+                ${curso.titulo}
+            </td>
+        `;
+
+        //Agrega el HTML del carrito en el tbody
+        contenedorCarrito.appendChild(row);
+    });
+}
+
+//Eliminar los cursos del tbody
+function limpiarHTML() {
+    //forma lenta
+    //contenedorCarrito.innerHTML = "";
+    while (contenedorCarrito.firstChild) {
+        contenedorCarrito.removeChild(contenedorCarrito.firstChild);
+    }
+}
